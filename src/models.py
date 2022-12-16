0ba5c2ad1905e75d876e88c8deb39e1841f94db9
Juan David <jdda886@gmail.com>
2022-12-16 00:56:42 +0000

    starwars data modeling

--- src/models.py
+++ src/models.py
@@ -1,38 +1,78 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+import enum
+from sqlalchemy import Integer, Enum
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Characters(Base):
+    __tablename__ = 'characters'
+    name = Column(String(250), ForeignKey("vehicle.pilot"), ForeignKey("favorites.favorite_characters"))
+    planet_from = Column(String(250), ForeignKey("planets.name"))
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    age = Column(Integer)
+    
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    size = Column(Integer)
+    population = Column(Integer)
+    climate = Column(String(250))
+    name = Column(String(250), ForeignKey("favorites.favorite_planets"))
 
-    def to_dict(self):
-        return {}
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), ForeignKey("favorites.favorite_vehicles"))
+    pilot = Column(String(250))
+    type = Column(String(250))
+
+class Users(Base):
+    __tablename__ = 'users'
+    username = Column(String(250), nullable=False)
+    pasword = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey("favorites.user_id"), primary_key=True)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    date_added = Column(DateTime(False))
+    user_id = Column(Integer, primary_key=True)
+    favorite_characters = Column(Integer)
+    favorite_planets = Column(Integer)
+    favorite_vehicles = Column(Integer)
+
+
+# class Post(Base):
+#     __tablename__ = 'post'
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey('user.id'))
+
+# class Comment(Base):
+#     __tablename__ = 'comment'
+#     id = Column(Integer, primary_key=True)
+#     comment_text = Column(String(250), nullable=False)
+#     author_id = Column(Integer, ForeignKey('user.id'))
+#     post_id = Column(Integer, ForeignKey('post.id'))
+
+# class MediaType(enum.Enum):
+#     png = "png"
+#     jpg = "jpg"
+#     gif = "gif"
+
+# class Media(Base):
+#     __tablename__ = 'media'
+#     id = Column(Integer, primary_key=True)
+#     type = Column(Enum(MediaType))
+#     url = Column(String(250), nullable=False)
+#     post_id = Column(Integer, ForeignKey('post.id'))
+    
+   
 
 ## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+render_er(Base, 'diagram.png')
